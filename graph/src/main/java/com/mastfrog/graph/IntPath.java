3549b7d2690cf87528ecb26a25ef3b5653decf60
Tim Boudreau <tim@timboudreau.com>
2020-08-23 03:29:53 -0400

    A few more minor improvements; docs; faster negative test for containment in IntPath; make set of methods more consistent between IntPath and ObjectPath

--- graph/src/main/java/com/mastfrog/graph/IntPath.java
+++ graph/src/main/java/com/mastfrog/graph/IntPath.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.function.IntConsumer;
 import com.mastfrog.abstractions.list.IndexedResolvable;
+import java.util.BitSet;
 
 /**
- * A path through a graph.
+ * A finite path between two elements in a graph.
  *
  * @author Tim Boudreau
  */
@@ -15,6 +16,7 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
     private static int DEFAULT_SIZE = 12;
     private int[] items;
     private int size;
+    private BitSet contents;
 
     private IntPath(int size, int[] items) {
         this.size = size;
@@ -33,26 +35,83 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return new IntPath(size, items);
     }
 
+    /**
+     * Create a new path prepending the passed value.
+     *
+     * @param value A value
+     * @return this
+     * @throws IllegalArgumentException if the value is negative
+     */
     public IntPath prepending(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Paths may not contain "
+                    + "negative elements: " + value);
+        }
         int[] nue = new int[size + 1];
         System.arraycopy(items, 0, nue, 1, items.length);
         nue[0] = value;
-        return new IntPath(nue.length, nue);
+        IntPath result = new IntPath(nue.length, nue);
+        if (contents != null) {
+            BitSet newContents = (BitSet) contents.clone();
+            newContents.set(value);
+            result.contents = newContents;
+        }
+        return result;
     }
 
+    /**
+     * Create a new path appending the passed value.
+     *
+     * @param value A value
+     * @return this
+     * @throws IllegalArgumentException if the value is negative
+     */
     public IntPath appending(int value) {
-        int[] nue = new int[size+1];
+        if (value < 0) {
+            throw new IllegalArgumentException("Paths may not contain "
+                    + "negative elements: " + value);
+        }
+        int[] nue = new int[size + 1];
         System.arraycopy(items, 0, nue, 0, items.length);
         items[size] = value;
-        return new IntPath(nue.length, nue);
+        IntPath result = new IntPath(nue.length, nue);
+        if (contents != null) {
+            BitSet newContents = (BitSet) contents.clone();
+            newContents.set(value);
+            result.contents = newContents;
+        }
+        return result;
     }
 
+    /**
+     * Iterate all elements of the path, passing them to the passed consumer.
+     *
+     * @param c A consumer
+     */
     public void forEachInt(IntConsumer c) {
         for (int i = 0; i < size; i++) {
             c.accept(items[i]);
         }
     }
 
+    /**
+     * Iterate all elements of the path in reverse order, passing them to the
+     * passed consumer.
+     *
+     * @param c A consumer
+     */
+    public void forEachIntReversed(IntConsumer c) {
+        for (int i = size - 1; i >= 0; i--) {
+            c.accept(items[i]);
+        }
+    }
+
+    /**
+     * Get the first path element
+     *
+     * @return The first path element
+     * @throws IndexOutOfBoundsException if out of range
+     */
     public int first() {
         if (size == 0) {
             throw new IndexOutOfBoundsException("Empty");
@@ -60,11 +119,17 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return items[0];
     }
 
+    /**
+     * Get the last path element
+     *
+     * @return The first path element
+     * @throws IndexOutOfBoundsException if out of range
+     */
     public int last() {
         if (size == 0) {
             throw new IndexOutOfBoundsException("empty");
         }
-        return items[size-1];
+        return items[size - 1];
     }
 
     private void growIfNeeded() {
@@ -73,28 +138,37 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         }
     }
 
-    public IntPath addAll(int... values) {
+    IntPath addAll(int... values) {
+        if (values.length == 0) {
+            return this;
+        }
         if (size + values.length < items.length) {
             items = Arrays.copyOf(items, items.length + values.length);
         }
         System.arraycopy(values, 0, items, size, values.length);
         size += values.length;
+        contents = null;
         return this;
     }
 
     IntPath add(int item) {
         growIfNeeded();
         items[size++] = item;
+        contents = null;
         return this;
     }
 
     IntPath append(IntPath other) {
+        if (other.isEmpty()) {
+            return this;
+        }
         int targetSize = size() + other.size();
         if (items.length < targetSize) {
             items = Arrays.copyOf(items, targetSize);
         }
         System.arraycopy(other.items, 0, items, size, other.size());
         size = targetSize;
+        contents = null;
         return this;
     }
 
@@ -103,6 +177,11 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return this;
     }
 
+    /**
+     * Create a new path, lopping off the first element.
+     *
+     * @return A new path
+     */
     public IntPath childPath() {
         if (size == 0) {
             return this;
@@ -112,6 +191,11 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return new IntPath(nue.length, nue);
     }
 
+    /**
+     * Create a new path, lopping off the last element.
+     *
+     * @return A new path
+     */
     public IntPath parentPath() {
         if (size == 0) {
             return this;
@@ -124,15 +208,23 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
     IntPath replace(int index, IntPath other) {
         size = index;
         append(other);
+        contents = null;
         return this;
     }
 
+    /**
+     * Create a new path whose elements are this one's in reverse order.
+     *
+     * @return A new path
+     */
     public IntPath reversed() {
         int[] nue = new int[size];
         for (int i = 0; i < size; i++) {
             nue[i] = items[size - (i + 1)];
         }
-        return new IntPath(size, nue);
+        IntPath result = new IntPath(size, nue);
+        result.contents = contents;
+        return result;
     }
 
     public int start() {
@@ -143,12 +235,27 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return size == 0 ? -1 : items[size - 1];
     }
 
+    /**
+     * Determine if this path contains the subsequence in the passed path, or is
+     * equal to it.
+     *
+     * @param path A path
+     * @return true if this path contains the passed one
+     */
     public boolean contains(IntPath path) {
-        if (path.size() > size) {
+        if (path == this) {
+            return true;
+        } else if (path.size() > size) {
             return false;
         } else if (path.size() == size) {
             return path.equals(this);
         } else {
+            BitSet ct = contents();
+            for (int i = 0; i < path.size; i++) {
+                if (!ct.get(path.get(i))) {
+                    return false;
+                }
+            }
             for (int i = 0; i < size - path.size(); i++) {
                 if (arraysEquals(items, i, i + path.size(), path.items, 0, path.size())) {
                     return true;
@@ -158,7 +265,18 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return false;
     }
 
-    public static boolean arraysEquals(int[] a, int aFromIndex, int aToIndex, int[] b, int bFromIndex, int bToIndex) {
+    private BitSet contents() {
+        if (contents != null) {
+            return contents;
+        }
+        BitSet result = new BitSet(size);
+        for (int i = 0; i < items.length; i++) {
+            result.set(items[i]);
+        }
+        return result;
+    }
+
+    static boolean arraysEquals(int[] a, int aFromIndex, int aToIndex, int[] b, int bFromIndex, int bToIndex) {
         // JDK 9
 //        return Arrays.equals(a, aFromIndex, aToIndex, b, bFromIndex, bToIndex);
         int aLength = aToIndex - aFromIndex;
@@ -174,32 +292,90 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return true;
     }
 
+    /**
+     * Get the first occurrence of the passed value in this path.
+     *
+     * @param val An integer path element
+     * @return the index or -1
+     */
     public int indexOf(int val) {
+        if (!contains(val)) {
+            return -1;
+        }
         for (int i = 0; i < size; i++) {
             if (get(i) == val) {
                 return i;
             }
         }
-        return -1;
+        throw new AssertionError("contents bitset out of sync");
     }
 
-    public boolean contains(int val) {
-        for (int i = 0; i < size; i++) {
-            if (val == get(i)) {
-                return true;
+    /**
+     * Get the first occurrence of the passed value in this path.
+     *
+     * @param val An integer path element
+     * @return the index or -1
+     */
+    public int lastIndexOf(int val) {
+        if (!contains(val)) {
+            return -1;
+        }
+        for (int i = size - 1; i >= 0; i--) {
+            if (get(i) == val) {
+                return i;
             }
         }
-        return false;
+        throw new AssertionError("contents bitset out of sync");
     }
 
+    /**
+     * Determine if this path contains the passed value.
+     *
+     * @param val A value
+     * @return true if it is present
+     */
+    public boolean contains(int val) {
+        if (val < 0) {
+            throw new IllegalArgumentException("Cannot contain negative values: " + val);
+        }
+        return contents().get(val);
+    }
+
+    /**
+     * Determine if this path is empty.
+     *
+     * @return True if it is empty
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Get the number of elements in this path.
+     *
+     * @return The number of elements
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Determine if this path does not represent an actual path - if it has one
+     * element or less.
+     *
+     * @return True if this path does not have an endpoint and may or may not
+     * have a start point
+     */
+    public boolean isNotAPath() {
+        return size() < 2;
+    }
+
+    /**
+     * Get the path element at the specified index.
+     *
+     * @param index The index within the path
+     * @return The index or -1 if not present
+     */
     public int get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException(index + " of " + size);
@@ -207,10 +383,22 @@ public final class IntPath implements Comparable<IntPath>, Iterable<Integer> {
         return items[index];
     }
 
+    /**
+     * Get the contents of this path as an int[].
+     *
+     * @return An array
+     */
     public int[] items() {
         return size == items.length ? items : Arrays.copyOf(items, size);
     }
 
+    /**
+     * Iterate the contents.
+     *
+     * @param cons
+     * @deprecated use the better-named <code>forEachInt()</code>
+     */
+    @Deprecated
     public void iterate(IntConsumer cons) {
         for (int i = 0; i < size; i++) {
             cons.accept(get(i));
--- graph/src/main/java/com/mastfrog/graph/ObjectPath.java
+++ graph/src/main/java/com/mastfrog/graph/ObjectPath.java
@@ -24,9 +24,13 @@
 package com.mastfrog.graph;
 
 import com.mastfrog.abstractions.list.IndexedResolvable;
+import com.mastfrog.bits.collections.BitSetSet;
 import java.lang.reflect.Array;
+import java.util.BitSet;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * One path in an object graph.
@@ -43,6 +47,13 @@ public final class ObjectPath<T> implements Iterable<T>, Comparable<ObjectPath> 
         this.indexed = indexed;
     }
 
+    /**
+     * Create a new path prepending the passed object.
+     *
+     * @param obj Another object, which must actually be contained in the
+     * original graph this path came from, or null will be returned
+     * @return A new path or null
+     */
     public ObjectPath prepending(T obj) {
         int ix = indexed.indexOf(obj);
         if (ix < 0) {
@@ -51,6 +62,13 @@ public final class ObjectPath<T> implements Iterable<T>, Comparable<ObjectPath> 
         return new ObjectPath<>(ip.prepending(ix), indexed);
     }
 
+    /**
+     * Create a new path appending the passed object.
+     *
+     * @param obj Another object, which must actually be contained in the
+     * original graph this path came from, or null will be returned
+     * @return A new path or null
+     */
     public ObjectPath appending(T obj) {
         int ix = indexed.indexOf(obj);
         if (ix < 0) {
@@ -59,39 +77,157 @@ public final class ObjectPath<T> implements Iterable<T>, Comparable<ObjectPath> 
         return new ObjectPath<>(ip.appending(ix), indexed);
     }
 
+    /**
+     * Get all of the contained objects as a set.
+     *
+     * @return A set
+     */
+    public Set<? extends T> contents() {
+        BitSet set = new BitSet(ip.size());
+        ip.forEachInt(set::set);
+        return new BitSetSet<>(indexed, set);
+    }
+
+    /**
+     * Get the number of elements in this path.
+     *
+     * @return The size
+     */
     public int size() {
         return ip.size();
     }
 
+    /**
+     * Returns the first element.
+     *
+     * @return The first element
+     * @throws NoSuchElementException if the path is empty
+     */
     public T first() {
+        if (ip.isEmpty()) {
+            throw new NoSuchElementException("empty path");
+        }
         return indexed.forIndex(ip.first());
     }
 
+    /**
+     * Returns the last element.
+     *
+     * @return The last element
+     * @throws NoSuchElementException if the path is empty
+     */
     public T last() {
+        if (ip.isEmpty()) {
+            throw new NoSuchElementException("empty path");
+        }
         return indexed.forIndex(ip.last());
     }
 
+    /**
+     * Get the path element at index i.
+     *
+     * @param i An index <code>&gt;=0</code> and <code>&lt size()</code>
+     * @return A path element
+     */
     public T get(int i) {
         int value = ip.get(i);
         return indexed.forIndex(value);
     }
 
+    /**
+     * Determine if this path contains the sequence of elements in another path.
+     *
+     * @param other Another path
+     * @return true if the passed path is equal to or a subpath of this one
+     */
     public boolean containsPath(ObjectPath<T> other) {
         return ip.contains(other.ip);
     }
 
+    /**
+     * Determine if this path contains the passed item.
+     *
+     * @param obj
+     * @return
+     */
     public boolean contains(T obj) {
         int ix = indexOf(obj);
         return ix >= 0;
     }
 
+    /**
+     * Get the (first) index of an item in this path.
+     *
+     * @param obj An object
+     * @return the index or -1 if not present
+     */
     public int indexOf(T obj) {
         int ix = indexed.indexOf(obj);
         return ix < 0 ? -1 : ip.indexOf(ix);
     }
 
+    /**
+     * Get the (first) index of an item in this path.
+     *
+     * @param obj An object
+     * @return the index or -1 if not present
+     */
+    public int lastIndexOf(T obj) {
+        int ix = indexed.indexOf(obj);
+        return ix < 0 ? -1 : ip.lastIndexOf(ix);
+    }
+
+    /**
+     * Get a copy of this path, lopping off the first element.
+     *
+     * @return A new path
+     */
+    public ObjectPath<T> childPath() {
+        if (isEmpty()) {
+            return this;
+        }
+        return new ObjectPath<>(ip.childPath(), indexed);
+    }
+
+    /**
+     * Get a copy of this path, lopping off the last element.
+     *
+     * @return A new path
+     */
+    public ObjectPath<T> parentPath() {
+        if (isEmpty()) {
+            return this;
+        }
+        return new ObjectPath<>(ip.childPath(), indexed);
+    }
+
+    /**
+     * Determine if this path is empty.
+     *
+     * @return True if the path contains no elements
+     */
     public boolean isEmpty() {
         return ip.isEmpty();
+    }
+
+    /**
+     * Determine if this path does not represent an actual path - if it has one
+     * element or less.
+     *
+     * @return True if this path does not have an endpoint and may or may not
+     * have a start point
+     */
+    public boolean isNotAPath() {
+        return ip.isNotAPath();
+    }
+
+    /**
+     * Create a new path whose elements are this one's in reverse order.
+     *
+     * @return A new path
+     */
+    public ObjectPath<T> reversed() {
+        return new ObjectPath<>(ip.reversed(), indexed);
     }
 
     public T start() {
