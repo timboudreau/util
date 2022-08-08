ac7eb00890e2252889bbfc602e6d43f3eb2eac68
Tim Boudreau <timb@telenav.com>
2022-08-08 15:19:02 -0400

    Add non-word-omitting escaper, add a few entities to basic html escaper

--- util-strings/src/main/java/com/mastfrog/util/strings/Escaper.java
+++ util-strings/src/main/java/com/mastfrog/util/strings/Escaper.java
@@ -215,17 +215,33 @@ public interface Escaper {
         switch (c) {
             case '"':
                 return "&quot;";
+            case '\'':
+                return "&apos";
             case '&':
                 return "&amp;";
             case '<':
                 return "&lt;";
             case '>':
                 return "&gt;";
+            case '©':
+                return "&copy;";
+            case '®':
+                return "&reg;";
+            case '\u2122':
+                return "&trade;";
+            case '¢':
+                return "&cent;";
+            case '£':
+                return "&pound;";
+            case '¥':
+                return "&yen;";
+            case '€':
+                return "&euro;";
             default:
                 return null;
         }
     };
-
+    
     /**
      * Escapes double quotes, ampersands, less-than and greater-than to their
      * SGML entities, and replaces \n with &lt;br&gt;.
@@ -286,5 +302,15 @@ public interface Escaper {
             default:
                 return null;
         }
+    };
+
+    /**
+     * Omits characters which are neither letters nor digits - useful
+     * for hash-matching text that may have varying amounts of whitespace
+     * or other non-semantic formatting differences.
+     */
+    public static Escaper OMIT_NON_WORD_CHARACTERS = c -> {
+        return !Character.isDigit(c) && !Character.isLetter(c) ? "" :
+                Character.toString(c);
     };
 }
